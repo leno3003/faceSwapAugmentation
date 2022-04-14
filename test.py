2168f845d951f0caea77035801653eb27a34822f
leno3003 <cassanoenrico53@gmail.com>
2022-04-14 12:30:17 +0200

    modified filename

---
+++ test.py
@@ -0,0 +1,6 @@
+USERS=['Amparore', 'Baccega', 'Basile', 'Beccuti', 'Botta', 'Castagno', 'Davide', 'DiCaro', 'DiNardo', 
+       'Esposito', 'Francesca', 'Giovanni', 'Gunetti', 'Idilio', 'Ines', 'Malangone', 'Maurizio', 
+       'Michael', 'MirkoLai', 'MirkoPolato', 'Olivelli', 'Pozzato', 'Riccardo', 'Rossana', 'Ruggero', 
+       'Sapino', 'Simone', 'Susanna', 'Theseider', 'Thomas' ]
+nome="Amparore"
+print(USERS.index(nome))
