d6512ce9d47836464cd8806d11f8f324b62885de
leno3003 <cassanoenrico53@gmail.com>
2022-04-26 19:23:29 +0200

    exit removed

--- test.py
+++ test.py
@@ -2,5 +2,24 @@ USERS=['Amparore', 'Baccega', 'Basile', 'Beccuti', 'Botta', 'Castagno', 'Davide'
        'Esposito', 'Francesca', 'Giovanni', 'Gunetti', 'Idilio', 'Ines', 'Malangone', 'Maurizio', 
        'Michael', 'MirkoLai', 'MirkoPolato', 'Olivelli', 'Pozzato', 'Riccardo', 'Rossana', 'Ruggero', 
        'Sapino', 'Simone', 'Susanna', 'Theseider', 'Thomas' ]
-nome="Francesca"
+nome="Davide"
 print(USERS.index(nome))
+print(USERS[0])
+print(USERS[1])
+print(USERS[3])
+print(USERS[7])
+print(USERS[9])
+print(USERS[10])
+print(USERS[11])
+print(USERS[12])
+print(USERS[13])
+print(USERS[15])
+print(USERS[16])
+print(USERS[17])
+print(USERS[18])
+print(USERS[19])
+print(USERS[23])
+print(USERS[25])
+print(USERS[26])
+print(USERS[27])
+print(USERS[28])
